350396b5baf4ca92218f84d10930ba0e397a7e60
Andrew Fitz Gibbon <fitz@temporal.io>
2022-11-10 17:32:52 -0800

    move psuedocode sketch to the activities.

--- temporal/activities.go
+++ temporal/activities.go
@@ -11,21 +11,51 @@ import (
 type Activities struct{}
 
 func (a *Activities) ValidateOrder(ctx context.Context, o *gopherpizza.PizzaOrderInfo) error {
+	// TODO: Replace with actual logic
 	time.Sleep(time.Second * time.Duration(rand.Intn(ACTIVITY_MOCK_DURATION_SEC)))
+
+	// check that the request has everything needed for a "valid" order
+	// check inventory for the requested ingredients
 	return nil
 }
 
 func (a *Activities) PreparePizza(ctx context.Context, o *gopherpizza.PizzaOrderInfo) error {
+	// TODO: Replace with actual logic
 	time.Sleep(time.Second * time.Duration(rand.Intn(ACTIVITY_MOCK_DURATION_SEC)))
+
+	// Random duration (short).
+	// Chance to fail with:
+	//  - Missing ingredients (non-retryable)
+	//      - Trying to get ingredients *should* succeed, given ValidateOrder above. Fail if it doesn't.
+	//  - Threw dough too high; it's stuck on the ceiling (infinitely retryable)
+	//  - Dropped dough; it's been stepped on (infinitely retryable)
+
 	return nil
 }
 
 func (a *Activities) BakePizza(ctx context.Context, o *gopherpizza.PizzaOrderInfo) error {
+	// TODO: Replace with actual logic
 	time.Sleep(time.Second * time.Duration(rand.Intn(ACTIVITY_MOCK_DURATION_SEC)))
+
+	// Activity: Random duration (long)
+	// Chance to fail with:
+	//  - Undercooked (retryable once)
+	//  - Overcooked (non-retryable)
+	//     - compensate by backtracking to `prepare`
 	return nil
 }
 
 func (a *Activities) Deliver(ctx context.Context, o *gopherpizza.PizzaOrderInfo) error {
+	// TODO: Replace with actual logic
 	time.Sleep(time.Second * time.Duration(rand.Intn(ACTIVITY_MOCK_DURATION_SEC)))
+
+	// Random duration (medium)
+	// Chance to fail with:
+	//  - Driver forgot address (infinitely retryable)
+	//  - Driver is lost ("soft" fail; activity takes longer than expected but is still heartbeating)
+	//  - Driver quit during delivery (worker process fails heartbeat; non-retryable)
+	//     - compensate by backtracking to `prepareRaw`
+	//  - Customer got wrong order (non-retryable)
+	//     - compensate by backtracking to `prepareRaw`
 	return nil
 }
--- temporal/workflow.go
+++ temporal/workflow.go
@@ -9,38 +9,6 @@ import (
 	"go.temporal.io/sdk/workflow"
 )
 
-/* Pseudocode:
-
-PizzaWorkflow(order):
-  // in-workflow: does the order request have all the requisite info?
-  // Fail the workflow if it doesn't. Continue otherwise.
-  validateOrder()
-
-  // Activity: Random duration (short).
-  // Chance to fail with:
-  //  - Missing ingredients (non-retryable)
-  //  - Threw dough too high; it's stuck on the ceiling (infinitely retryable)
-  //  - Dropped dough; it's been stepped on (infinitely retryable)
-	prepareRaw()
-
-  // Activity: Random duration (long)
-  // Chance to fail with:
-  //  - Undercooked (retryable once)
-  //  - Overcooked (non-retryable)
-  //     - compensate by backtracking to `prepareRaw`
-	bake()
-
-  // Activity: Random duration (medium)
-  // Chance to fail with:
-  //  - Driver forgot address (infinitely retryable)
-  //  - Driver is lost ("soft" fail; activity takes longer than expected but is still heartbeating)
-  //  - Driver quit during delivery (worker process fails heartbeat; non-retryable)
-  //     - compensate by backtracking to `prepareRaw`
-  //  - Customer got wrong order (non-retryable)
-  //     - compensate by backtracking to `prepareRaw`
-  deliver()
-*/
-
 var buildStatus gopherpizza.OrderStatus = gopherpizza.OrderStatus_ORDER_RECEIVED
 
 const ACTIVITY_MOCK_DURATION_SEC int = 60
@@ -80,21 +48,21 @@ func PizzaWorkflow(ctx workflow.Context, o *gopherpizza.PizzaOrderInfo) error {
 		return err
 	}
 
-	err = workflow.ExecuteActivity(ctx, a.ValidateOrder, a.Order).Get(ctx, nil)
+	err = workflow.ExecuteActivity(ctx, a.ValidateOrder, o).Get(ctx, nil)
 	if err != nil {
 		log.Error("Step failed", "Err", err)
 		return err
 	}
 
 	buildStatus = gopherpizza.OrderStatus_ORDER_PREPARING
-	err = workflow.ExecuteActivity(ctx, a.PreparePizza, a.Order).Get(ctx, nil)
+	err = workflow.ExecuteActivity(ctx, a.PreparePizza, o).Get(ctx, nil)
 	if err != nil {
 		log.Error("Step failed", "Err", err)
 		return err
 	}
 
 	buildStatus = gopherpizza.OrderStatus_ORDER_BAKING
-	err = workflow.ExecuteActivity(ctx, a.BakePizza, a.Order).Get(ctx, nil)
+	err = workflow.ExecuteActivity(ctx, a.BakePizza, o).Get(ctx, nil)
 	if err != nil {
 		log.Error("Step failed", "Err", err)
 		return err
@@ -102,7 +70,7 @@ func PizzaWorkflow(ctx workflow.Context, o *gopherpizza.PizzaOrderInfo) error {
 	buildStatus = gopherpizza.OrderStatus_ORDER_PENDING_PICKUP
 
 	buildStatus = gopherpizza.OrderStatus_ORDER_OUT_FOR_DELIVERY
-	err = workflow.ExecuteActivity(ctx, a.Deliver, a.Order).Get(ctx, nil)
+	err = workflow.ExecuteActivity(ctx, a.Deliver, o).Get(ctx, nil)
 	if err != nil {
 		log.Error("Step failed", "Err", err)
 		return err
